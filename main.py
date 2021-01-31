fc8844756f5db0bb400bce682d33c2db39ab0b76
wtfnukee <wtfnukee@gmail.com>
2021-01-31 22:32:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ class BotHandler:
             last_update = get_result[len(get_result) - 1]
 
         print(last_update)
-        with open('text.txt', 'w') as f:
+        with open('text.txt', 'a') as f:
             f.write(last_update)
 
         return last_update
